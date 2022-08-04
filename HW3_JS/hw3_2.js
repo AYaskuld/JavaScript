5f4ee60a82d8cbd11407c2af010e0f3d528c4450
Artyom Yaskuld <98359811+AYaskuld@users.noreply.github.com>
2022-08-04 21:51:37 +0300

    Update hw3_2.js

--- HW3_JS/hw3_2.js
+++ HW3_JS/hw3_2.js
@@ -8,22 +8,25 @@
 const fs = require("fs");
 //подключение файла
 let user = JSON.parse(fs.readFileSync("D:/Projects/HW3/task2.json"))
-/*//фильтрация масссива
+
+//Первый способ
 function uniqueFunc1() {
     let unique1 = []
     user.forEach(el => {
         //проверка того, что unique1 не содержит элементы из массива user
-        //т.к includes плохо видит вложенные объекты, то приводим el к string(JSON.stringify)
+        //т.к includes плохо видит вложенные объекты, то приводим el к строке(JSON.stringify)
         if (!unique1.includes(JSON.stringify(el)))
             unique1.push(JSON.stringify(el))
     })
     // преобразование строки unique1 из функции в формат Json
     return unique1.map(el => JSON.parse(el))
 }
-console.log(uniqueFunc1()) */
+//console.log(uniqueFunc1()) 
 //___________________________________________________________________________________________
 
-//1.(new Set(users.map((item) =>JSON.stringify(item))))- приведение в string значений из users и педача в Set(не массив)
+//Второй способ
+
+//1.(new Set(users.map((item) =>JSON.stringify(item))))- приведение в string значений из users и педача в Set(не массив). Set() создает набор уникальных значений.
 //2.Создаем массив из Set - Array.from(
 //3.И т.к. в полученном массиве тип дпнных string, то преобразовываем обратно в Json(.map((item) => JSON.parse(item)))  
 let unique = Array.from(new Set(user.map((item) => JSON.stringify(item)))).map((item) => JSON.parse(item))
